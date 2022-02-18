ac390d3feedc69efce959bd71ac1c2a237cd3216
Jordan Vidrine <jordan@jordanvidrine.com>
2022-02-17 20:00:50 -0600

    moment

--- .eleventy.js
+++ .eleventy.js
@@ -1,18 +1,21 @@
 const markdownIt = require("markdown-it");
 const markdownItAnchor = require("markdown-it-anchor");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const moment = require('moment');
 
 const now = String(Date.now());
 
 module.exports = function (eleventyConfig) {
-  // Output directory: _site
-
   // Copy `assets/` to `_site/assets`
   eleventyConfig.addPassthroughCopy("assets");
 
   eleventyConfig.addShortcode("version", function () {
     return now;
   });
+
+  eleventyConfig.addFilter('dateIso', date => {
+    return moment(date).utc().format('LL'); // May 31, 2019
+  })
 
   // excerpt parsing
   eleventyConfig.setFrontMatterParsingOptions({
