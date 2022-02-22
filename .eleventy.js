98d6d4aa855093000c999d598fc0cdd4780dc94b
Jordan Vidrine <jordan@jordanvidrine.com>
2022-02-22 17:09:53 -0600

    Cleanup

--- .eleventy.js
+++ .eleventy.js
@@ -3,15 +3,9 @@ const markdownItAnchor = require("markdown-it-anchor");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const moment = require("moment");
 
-const now = String(Date.now());
-
 module.exports = function (eleventyConfig) {
   // Copy `assets/` to `_site/assets`
   eleventyConfig.addPassthroughCopy("assets");
-
-  eleventyConfig.addShortcode("version", function () {
-    return now;
-  });
 
   eleventyConfig.addFilter("dateIso", date => {
     return moment(date).utc().format("LL"); // May 31, 2019
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Page Header
+### Jordan Vidrine Personal Site
+This repo is for my website at [www.jordanvidrine.com](www.jordanvidrine.com)
+
+Built by me with ❤️ using [11ty](https://www.11ty.dev/)
